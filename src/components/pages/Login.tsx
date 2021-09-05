1f1d97c8f49795a93c51a4db38feb5747bc1992d
AKIHISA SHIMADA <empereur@empereur-2.local>
2021-09-05 18:45:26 +0900

    create Login and API

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,8 +1,18 @@
-import { memo, VFC } from "react"
+import { memo, ChangeEvent, useState, VFC } from "react"
 import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import { PrimaryButtoh } from "../../atomos/button/PrimaryButton";
+import { useAuth } from "../../hooks/useAuth";
 
 export const Login: VFC = memo(() => {
+  const { login, loading} = useAuth();
+
+  const [userId, setUserId] = useState('')
+
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+  }
+
+  const onClickLogin = () => login(userId);
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -10,8 +20,14 @@ export const Login: VFC = memo(() => {
         <Divider my={4} />
 
         <Stack spacing={6} py={4} px={10}>
-          <Input placeholder="ユーザID" />
-          <PrimaryButtoh>ログイン</PrimaryButtoh>
+          <Input placeholder="ユーザID" value={userId} onChange={onChangeUserId} />
+          <PrimaryButtoh
+            disabled={userId === "" || loading}
+            loading={loading}
+            onClick={onClickLogin}
+          >
+            ログイン
+          </PrimaryButtoh>
         </Stack>
       </Box>
     </Flex>
